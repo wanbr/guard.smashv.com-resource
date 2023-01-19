22e453131a4c172e4d5a25151e91df55b2efef48
Thiago Neubern <76756810+ThiagoDEV17@users.noreply.github.com>
2023-01-19 01:14:09 -0300

    wip: Starting add token verification and bootstrap function

--- src/server/index.lua
+++ src/server/index.lua
@@ -4,3 +4,24 @@ if not TOKEN or TOKEN == '' then
     error('SMASHV_GUARD_TOKEN is not set')
     return
 end
+
+local function validate_token()
+    local validated = false
+
+    PerformHttpRequest('https://smashv-guard.herokuapp.com/validate', function(err, text, headers)
+        print(err, text, headers)
+    end, 'POST', json.encode({ token = TOKEN }), { ['Content-Type'] = 'application/json' })
+
+    return validated
+end
+
+local function bootstrap()
+    local validated = validate_token()
+
+    if not validated then
+        error('SMASHV_GUARD_TOKEN is invalid')
+        return
+    end
+end
+
+bootstrap()
