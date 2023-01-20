35116437a17045e08d64d83895626edd16f5583e
Thiago Neubern <76756810+ThiagoDEV17@users.noreply.github.com>
2023-01-20 00:20:47 -0300

    feat: Create a single file for resource global constants

---
+++ src/server/lib/constants.lua
@@ -0,0 +1,6 @@
+Constants = {}
+
+Constants.TOKEN = GetConvar('SMASHV_GUARD_TOKEN', nil)
+Constants.LOG_LEVEL = GetConvarInt("log_level", 4)
+
+Constants.API_BASE_URL = 'http://localhost:3000/api/fivem'
