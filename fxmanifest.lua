c0003898ae08291ddd31f534c961426db0834c6e
Thiago Neubern <76756810+ThiagoDEV17@users.noreply.github.com>
2023-01-20 00:21:03 -0300

    fix: Change server_scripts to a table

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,6 +7,8 @@ author 'SmashV'
 description 'SmashV Guard integration resource for FiveM'
 version '1.0.0'
 
-server_scripts 'src/**/*.lua'
+server_scripts {
+    'src/**/*.lua',
+}
 
 server_only 'yes'
